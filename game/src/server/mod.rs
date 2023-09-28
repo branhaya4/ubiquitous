0e0666314dba85694adeb7acf811c3233deae0ce
branhaya4 <83571677+branhaya4@users.noreply.github.com>
2023-09-28 11:16:45 -0700

    feat: add Devices

--- game/src/server/mod.rs
+++ game/src/server/mod.rs
@@ -1,14 +1,16 @@
+mod dev;
 mod fs;
 mod security;
 
-use std::fmt::Display;
-
+pub use dev::Device;
 pub use fs::{File, FileSystem};
 pub use security::{AttackInfo, ServerSecurity};
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub struct Server {
     pub name: String,
     pub fs: FileSystem,
     pub sec: ServerSecurity,
+    pub devices: Vec<Device>,
 }
