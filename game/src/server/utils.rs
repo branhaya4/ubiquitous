47d0d7c87a4189fd4ba46f7a97bb51c280f46df1
Dworv YT <dwarvyt@gmail.com>
2023-10-05 11:27:14 -0700

    feat: first hack commit

--- game/src/server/utils.rs
+++ game/src/server/utils.rs
@@ -1,7 +1,31 @@
 use petgraph::graph::NodeIndex;
 
-use super::Server;
+use super::{AttackInfo, SecurityState, Server};
 use crate::Sector;
+
+pub fn hack(
+    sector: &mut Sector,
+    index: NodeIndex,
+    attack: AttackInfo,
+    name: String,
+) -> Option<bool> {
+    let mut target_index = None;
+    for neighbor_index in sector.neighbors(index) {
+        if sector.node_weight(neighbor_index).unwrap().name == name {
+            target_index = Some(neighbor_index);
+            break;
+        }
+    }
+    target_index.map(|target| {
+        let security = &mut sector.node_weight_mut(target).unwrap().sec;
+        if security.can_compromise(&attack) {
+            security.state = SecurityState::Compromised;
+            true
+        } else {
+            false
+        }
+    })
+}
 
 pub fn ls(sector: &Sector, index: NodeIndex) -> Vec<String> {
     let server = sector.node_weight(index).unwrap();
--- game/src/terminal.rs
+++ game/src/terminal.rs
@@ -2,7 +2,7 @@ use std::io::stdin;
 
 use crate::{
     gen::gen_sector,
-    server::{utils, File, Server},
+    server::{utils, AttackInfo, AttackKind, File, Server},
     State,
 };
 
@@ -16,6 +16,7 @@ impl Terminal {
             state: State {
                 sectors: vec![gen_sector(1.)],
                 selected: (0, 0),
+                skills: [1.; 4],
             },
         }
     }
@@ -29,6 +30,46 @@ impl Terminal {
                 let (sector, server) = self.state.selected;
                 let sector = &mut self.state.sectors[sector];
                 match command {
+                    "hack" => {
+                        if let Some(kind) = words.next() {
+                            if let Some(name) = words.next() {
+                                let attack = match kind {
+                                    "pwd" | "password" | "crack" => {
+                                        Some(AttackInfo { kind: AttackKind::Password, skill: self.state.skills[0] })
+                                    }
+                                    "proto" | "protomanip" => {
+                                        Some(AttackInfo { kind: AttackKind::ProtoManip, skill: self.state.skills[1] })
+                                    }
+                                    "impersonation" | "phish" | "phishing" => {
+                                        Some(AttackInfo { kind: AttackKind::Impersonation, skill: self.state.skills[2] })
+                                    }
+                                    "collision" | "hash" | "hashing" => {
+                                        Some(AttackInfo { kind: AttackKind::Impersonation, skill: self.state.skills[3] })
+                                    }
+                                    _ => {
+                                        dbg!();
+                                        None
+                                    }
+                                };
+                                if let Some(attack) = attack {
+                                    if utils::hack(
+                                        sector,
+                                        server.into(),
+                                        attack,
+                                        name.to_string(),
+                                    ).unwrap() {
+                                        println!("hacking successful");
+                                    } else {
+                                        println!("hacking failed");
+                                    }
+                                } else {
+                                    println!("accepts kinds of attacks are password, ");
+                                }
+                            }
+                        } else {
+                            println!("usage: hack <kind> <server_name>");
+                        }
+                    }
                     "ls" => {
                         for name in utils::ls(sector, server.into()) {
                             println!("{}", name);
